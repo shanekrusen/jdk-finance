88d96ec36f05096b6acd3306c40fbe8a620e81dd
Shane Krusen <shanekrusen@gmail.com>
2017-04-22 00:55:56 -0400

    Initial commit to branch to save work while attempting a different route altogether

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,23 @@
 class User < ApplicationRecord
+  attr_accessor :phone
+  validates :phone, :length => { :is => 10 }
+  validates :phone, presence: true
+  validates :password, :length => { :is => 4 }
+  validates :password, :numericality => true
+
+
   has_many :incomes
   has_many :categories
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
+  end
 end
---
+++ db/migrate/20170422003835_change_password_to_integer.rb
@@ -0,0 +1,4 @@
+class ChangePasswordToInteger < ActiveRecord::Migration[5.0]
+  def change
+  end
+end
